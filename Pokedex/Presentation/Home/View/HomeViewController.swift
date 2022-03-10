533702ee5cf935eed8e53a4d74bab73e0ad94873
wspr <wspr@gft.com>
2022-03-10 15:19:35 -0300

    Create home screen

---
+++ Pokedex/Presentation/Home/View/HomeViewController.swift
@@ -0,0 +1,188 @@
+//
+//  HomeViewController.swift
+//  Pokedex
+//
+//  Created by Pinto Junior, William James on 10/03/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // MARK: - Constrants
+    let resuseIdentifier = "PokemonCollectionViewCell"
+    
+    // MARK: - Variables
+    // MARK: - Components
+    fileprivate let stackBase: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 32
+        stack.distribution = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    fileprivate let labelTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Pokédex"
+        label.font = UIFont(name: "OpenSans-Bold", size: 26)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let labelText: UILabel = {
+        let label = UILabel()
+        label.text = "Search for a Pokémon by name or using its"
+        label.font = UIFont(name: "Roboto-Regular", size: 17)
+        label.textColor = .lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let labelSubText: UILabel = {
+        let label = UILabel()
+        label.text = "National Pokédex number"
+        label.font = UIFont(name: "Roboto-Regular", size: 17)
+        label.textColor = .lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let textFieldSearch: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Name or number"
+        textField.backgroundColor = UIColor(red: 0.91, green: 0.95, blue: 0.95, alpha: 1)
+        textField.layer.cornerRadius = 8.0
+        textField.setLeftPaddingPoints(15)
+        textField.setRightPaddingPoints(15)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    fileprivate let buttonSearch: UIButton  = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 0.32, green: 0.33, blue: 0.44, alpha: 1)
+        button.setImage(UIImage(systemName: "line.3.horizontal.decrease"), for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 8.0
+        return button
+    }()
+    
+    fileprivate var pokedexCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsVerticalScrollIndicator = false
+        return collectionView
+    }()
+    
+    fileprivate func stackHeader() -> UIStackView {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 8
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.addArrangedSubview(labelTitle)
+        stack.addArrangedSubview(stackTitle())
+        return stack
+    }
+    
+    fileprivate func stackTitle() -> UIStackView {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.addArrangedSubview(labelText)
+        stack.addArrangedSubview(labelSubText)
+        return stack
+    }
+
+    fileprivate func stackSearch() -> UIStackView {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 16
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.addArrangedSubview(textFieldSearch)
+        stack.addArrangedSubview(buttonSearch)
+        return stack
+    }
+    
+    // MARK: - Lifecycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVC()
+    }
+    
+    // MARK: - Setup
+    fileprivate func setupVC() {
+        view.backgroundColor = .white
+        buildHierarchy()
+        buildConstraints()
+        setupCollection()
+    }
+    
+    fileprivate func setupCollection() {
+        pokedexCollectionView.dataSource = self
+        pokedexCollectionView.delegate = self
+        pokedexCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: resuseIdentifier)
+    }
+    
+    // MARK: - Methods
+    fileprivate func buildHierarchy() {
+        view.addSubview(stackBase)
+        stackBase.addArrangedSubview(stackHeader())
+        stackBase.addArrangedSubview(stackSearch())
+        stackBase.addArrangedSubview(pokedexCollectionView)
+    }
+    
+    fileprivate func buildConstraints() {
+        NSLayoutConstraint.activate([
+            stackBase.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            stackBase.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackBase.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackBase.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            textFieldSearch.heightAnchor.constraint(equalToConstant: 50),
+            buttonSearch.widthAnchor.constraint(equalToConstant: 50),
+
+        ])
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        return
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: resuseIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 2
+        return CGSize(width: width - 10, height: 250)
+    }
+}
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
